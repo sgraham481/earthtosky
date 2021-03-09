0bab519535d5e5258b48d7c94406dc73744ef11a
Sean Graham <sean.graham@ssaihq.com>
2021-03-08 19:18:34 -0500

    check orientation for full screen

--- js/main.js
+++ js/main.js
@@ -11,6 +11,11 @@ $(function(){
 
 window.addEventListener("orientationchange", function(event) {
   console.log("the orientation of the device is now " + event.target.screen.orientation.angle);
+  if (event.target.screen.orientation.angle){ /* 0 = portrait, 90 = landscape */
+  	closeFullscreen();
+  } else {
+  	openFullscreen();
+  }
 });
 
 function checkMobile(){
@@ -30,22 +35,26 @@ var elem = document.documentElement;
 
 /* View in fullscreen */
 function openFullscreen() {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
+	if (isMobile){
+	  if (elem.requestFullscreen) {
+	    elem.requestFullscreen();
+	  } else if (elem.webkitRequestFullscreen) { /* Safari */
+	    elem.webkitRequestFullscreen();
+	  } else if (elem.msRequestFullscreen) { /* IE11 */
+	    elem.msRequestFullscreen();
+	  }
+	}
 }
 
 /* Close fullscreen */
 function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) { /* Safari */
-    document.webkitExitFullscreen();
-  } else if (document.msExitFullscreen) { /* IE11 */
-    document.msExitFullscreen();
-  }
+	if (isMobile){
+	  if (document.exitFullscreen) {
+	    document.exitFullscreen();
+	  } else if (document.webkitExitFullscreen) { /* Safari */
+	    document.webkitExitFullscreen();
+	  } else if (document.msExitFullscreen) { /* IE11 */
+	    document.msExitFullscreen();
+	  }
+	}
 }
